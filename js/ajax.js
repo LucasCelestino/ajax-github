d7fe284190041da8aa0562846501a2a4882f695c
Lucas Celestino <lucascap899@gmail.com>
2023-03-21 18:24:17 -0300

    refatorando com promise

--- js/ajax.js
+++ js/ajax.js
@@ -2,49 +2,62 @@ let input = document.querySelector('input[name=github_user]');
 let button = document.querySelector('#btn_send');
 let div = document.querySelector('.app');
 
+let promise = () => {
+    return new Promise((resolve, reject) => {
+        let ajax = new XMLHttpRequest();
+    
+        ajax.open('GET', `https://api.github.com/users/${input.value}`);
+    
+        ajax.send(null);
+    
+        ajax.onreadystatechange = () => {
+    
+            if(ajax.readyState == 4)
+            {
+                if(ajax.status == 200)
+                {
+                    resolve(JSON.parse(ajax.responseText));
+                }
+                else
+                {
+                    reject(`Não foi possível encontrar um usuário com o nome: ${input.value}`);
+                }
+    
+                input.value = '';
+            }
+    
+        };
+    });
+};
+
 button.addEventListener('click', () => {
+
     div.innerHTML = '';
 
-    let ajax = new XMLHttpRequest();
+    let spanName = document.createElement('span');
 
-    ajax.open('GET', `https://api.github.com/users/${input.value}`);
+    let txtName = '';
 
-    ajax.send(null);
+    promise()
+    .then((user) => {
+        txtName = document.createTextNode(user['name']);
 
-    ajax.onreadystatechange = () => {
+        spanName.appendChild(txtName);
 
-        let spanName = document.createElement('span');
+        let imgUser = document.createElement('img');
+        imgUser.setAttribute('src', user['avatar_url']);
+        imgUser.setAttribute('alt', user['name']);
+        imgUser.width = '150';
+        imgUser.height = '150';
 
-        let txtName = '';
+        div.appendChild(imgUser);
+        div.appendChild(spanName);
+    })
+    .catch((error) => {
+        txtName = document.createTextNode(error);
+        spanName.appendChild(txtName);
+        div.appendChild(spanName);
+    });
 
-        if(ajax.readyState == 4)
-        {
-            if(ajax.status == 200)
-            {
-                user = JSON.parse(ajax.responseText);
-
-                txtName = document.createTextNode(user['name']);
-
-                spanName.appendChild(txtName);
-
-                let imgUser = document.createElement('img');
-                imgUser.setAttribute('src', user['avatar_url']);
-                imgUser.setAttribute('alt', user['name']);
-                imgUser.width = '150';
-                imgUser.height = '150';
-
-                div.appendChild(imgUser);
-                div.appendChild(spanName);
-            }
-            else
-            {
-                txtName = document.createTextNode(`Não foi possível encontrar um usuário com o nome: ${input.value}`);
-                spanName.appendChild(txtName);
-                div.appendChild(spanName);
-            }
-
-            input.value = '';
-        }
-
-    };
+    input.value = '';
 });
